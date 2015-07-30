966eb6aaf721c07bf761b9c5cea56df87483d760
Correção de bugs.lucasmfe <lucasmfe@localhost>
2015-07-30 13:46:15 -0300

    first commit

--- conection.php
+++ conection.php
@@ -5,7 +5,7 @@ $port = getenv("OPENSHIFT_MYSQL_DB_PORT");
 $conexao = mysql_connect($host . ":" . $port, "adminI4fDcIA", "r-rrAbGm1RHg") or die ("sem chance");
 //  $conexao = mysql_connect("localhost", "user_banco", "emocao") or die ("sem chance");
    
- $db = mysql_select_db("banco_jogo") or die("erro ao conectar banco");
+ $db = mysql_select_db("site") or die("erro ao conectar banco");
     mysql_query("SET NAMES 'utf8'");
         mysql_query('SET character_set_connection=utf8');
         mysql_query('SET character_set_client=utf8');
